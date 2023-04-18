3a36dda4ea0dfdeeecd69ac232de5cf0a5b3b725
Boshi <boshi.litw@gmail.com>
2023-04-18 11:18:10 +0800

    Corrected issue where the AI response was being replaced by the subsequent word

--- static/main.js
+++ static/main.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io.connect('http://' + document.domain + ':' + location.port);
     const messageInput = document.getElementById('message');
+    const sendButton = document.getElementById('send-button');
     const messagesList = document.getElementById('messages');
+
+    const sendMessage = () => {
+        if (messageInput.value) {
+            socket.send(messageInput.value);
+            messageInput.value = '';
+        }
+    };
+
     socket.on('connect', () => {
         console.log('connected');
         messageInput.addEventListener('keydown', (event) => {
-
-            if (event.key === 'Enter' && messageInput.value) {
-                socket.send(messageInput.value);
-                messageInput.value = '';
+            if (event.key === 'Enter') {
+                sendMessage();
             }
+        });
+
+        sendButton.addEventListener('click', () => {
+            sendMessage();
         });
     });
 
@@ -26,12 +37,12 @@ document.addEventListener('DOMContentLoaded', () => {
             // Otherwise, create a new list item and append it to the messages list
             listItem = document.createElement('li');
             listItem.id = messageId;
+            listItem.className = `${wrappedMessage.role}-message`;
             listItem.textContent = `[${wrappedMessage.timestamp}] ${wrappedMessage.message}`;
             messagesList.appendChild(listItem);
         }
 
         messagesList.scrollTop = messagesList.scrollHeight;
     });
-
 
 });
